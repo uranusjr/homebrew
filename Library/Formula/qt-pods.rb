9580b071639094a167fb6d99a6546460742c532e
Tzu-ping Chung <uranusjr@gmail.com>
2015-03-20 14:07:55 +0800

    Add primitive QtPods formula

---
+++ Library/Formula/qt-pods.rb
@@ -0,0 +1,24 @@
+class QtPods < Formula
+  homepage 'http://qt-pods.org'
+  head 'https://github.com/qt-pods/qt-pods.git'
+
+  depends_on 'qt5'
+  depends_on 'git' => :recommended
+
+  def install
+    # Can't perform parallel builds just yet.
+    # See https://github.com/qt-pods/qt-pods/issues/25
+    ENV.deparallelize
+
+    system "#{Formula['qt5'].bin}/qmake", "PREFIX=#{prefix}"
+    system 'make'
+    system 'make', 'install'
+  end
+
+  test do
+    # This is bad. A good test should generate a dummy project with
+    # Git-repository-like structure, and run qt-pods-cli in it. But
+    # this will do for now.
+    system "#{bin}/qt-pods-cli", '--version'
+  end
+end
